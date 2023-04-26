2c5e840cf2ba4b5146f258288363d7263e2278ad
Domunim <dk.kempa@gmail.com>
2023-04-26 18:58:20 +0200

    HomeViewModel adjusted to Asset model, minor UI tweaks

--- AurumMarket.WPF/ViewModels/MetalIndexViewModel.cs
+++ AurumMarket.WPF/ViewModels/MetalIndexViewModel.cs
@@ -9,10 +9,57 @@ using System.Threading.Tasks;
 
 namespace AurumMarket.WPF.ViewModels
 {
-    public class MetalIndexViewModel
+    public class MetalIndexViewModel : ViewModelBase
     {
         private readonly IMetalIndexServices _metalIndexServices;
-        public MetalIndexModel requestedIndex { get; set; }
+
+        private MetalIndexModel _gold;
+
+        public MetalIndexModel Gold
+        {
+            get { return _gold; }
+            set
+            {
+                _gold = value;
+                OnPropertyChanged(nameof(Gold));
+            }
+        }
+
+        private MetalIndexModel _silver;
+
+        public MetalIndexModel Silver
+        {
+            get { return _silver; }
+            set
+            {
+                _silver = value;
+                OnPropertyChanged(nameof(Silver));
+            }
+        }
+
+        private MetalIndexModel _platinum;
+
+        public MetalIndexModel Platinum
+        {
+            get { return _platinum; }
+            set
+            {
+                _platinum = value;
+                OnPropertyChanged(nameof(Platinum));
+            }
+        }
+
+        private MetalIndexModel _palladium;
+
+        public MetalIndexModel Palladium
+        {
+            get { return _palladium; }
+            set
+            {
+                _palladium = value;
+                OnPropertyChanged(nameof(Palladium));
+            }
+        }
 
         public MetalIndexViewModel(IMetalIndexServices metalIndexServices)
         {
